0799dd61d5f204d752d75abff3fdde0f935a7d4f
Apif Supriadi <afs.supriadi@gmail.com>
2020-07-10 22:01:28 +0700

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-   $server = "localhost";  
-   $username = "root";  
-   $password = "";  
-   $database = "sentimendb";  
+   $server = "rohini.iixcp.rumahweb.com";  
+   $username = "prex1283_mimin";  
+   $password = "YbwzcQDQ1-Qe";  
+   $database = "prex1283_sentimendb";  
    $connect = mysqli_connect($server, $username, $password) or die("Koneksi gagal");  
    mysqli_select_db($connect, $database) or die(mysqli_error($connect));    
 ?>
